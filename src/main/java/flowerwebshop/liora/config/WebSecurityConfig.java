97ab4f7e191117dc473b088fb005785390370c52
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-06-26 23:04:12 +0200

    edited a cors config and web security

--- src/main/java/flowerwebshop/liora/config/WebSecurityConfig.java
+++ src/main/java/flowerwebshop/liora/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package flowerwebshop.liora.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +17,11 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.DELETE, "/product/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/product/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/product").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/product/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/product").permitAll()
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session
