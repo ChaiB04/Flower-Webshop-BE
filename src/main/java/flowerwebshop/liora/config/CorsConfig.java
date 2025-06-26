8b3c1aa7b86f88757cda066537c98ce927d4d5e1
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-06-26 23:29:36 +0200

    added the deployed link of the frontend

--- src/main/java/flowerwebshop/liora/config/CorsConfig.java
+++ src/main/java/flowerwebshop/liora/config/CorsConfig.java
@@ -41,7 +41,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedHeaders(List.of("*", "https://flower-webshop-fe.vercel.app/"));
         config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
