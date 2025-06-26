37a96627bbee182e8abdff78fc38d2a3c6169901
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-06-26 23:18:27 +0200

    edited a cors config and web security

--- src/main/java/flowerwebshop/liora/config/CorsConfig.java
+++ src/main/java/flowerwebshop/liora/config/CorsConfig.java
@@ -39,7 +39,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "*"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
